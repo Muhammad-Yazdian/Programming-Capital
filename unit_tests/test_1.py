73fe7f14e66519b66f4872cd2320c5c41f0d8e08
Muhammad-Yazdian <93126501+Muhammad-Yazdian@users.noreply.github.com>
2022-02-04 11:23:56 +0100

    Update test_1.py

--- unit_tests/test_1.py
+++ unit_tests/test_1.py
@@ -3,12 +3,12 @@ from physics.particles_1d_line_segment_03_02 import func2 as f
 
 # content of test_sample.py
 def func(x):
-    return x + 1
+    return x + 1 
 
 
 def test_answer():
     assert func(3) == 4
 
 # def test_answer2():
-#     assert f.func2(3) == 4
+#     assert f.func2(3) == 4  
 
